1e6976d1cb27d325ff7290875455db152de65c3d
Senni Kaipio <senni.kaipio@protonmail.com>
2019-02-03 18:40:46 +0200

    Initial basis with app title at top

---
+++ src/components/TopBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TopBar = () => {
+  return (
+    <header className="mdc-top-app-bar mdc-top-app-bar--prominent">
+      <div className="mdc-top-app-bar__row">
+        <section className="mdc-top-app-bar__section">
+          <span className="mdc-top-app-bar__title">Isomorfia</span>
+        </section>
+      </div>
+    </header>
+  );
+};
+
+export default TopBar;
